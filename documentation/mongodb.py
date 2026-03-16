d2746dd989b5117084a7b2857eec6758e2960298
mukul2005kashyap <mukulkashyap4137@gmail.com>
2026-03-16 23:30:23 +0530

    add last task

---
+++ documentation/mongodb.py
@@ -0,0 +1,17 @@
+# mongo db
+
+"""
+mongo db is the open source document oriented nosql database design that is used to store and manage the large 
+volume of the databse effeciently using a flexible json flormate document 
+
+    -mongo db store the data in BSON formate to effecently handle the json formate document 
+    -it support dynamic schema to making it suitabel for unstructured and semistructured data
+    -scalable and easy to learn and widly used in modern framework like social midea and caching etc ...
+    -open dource and widley availabe on linux , windows and ios os ..
+
+Developers use MongoDB because it offers a lot of features for those who like to work with big data. 
+Here are few key reasons of why you should learn MongoDB.
+
+
+
+ """
